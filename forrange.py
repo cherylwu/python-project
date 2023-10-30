66f2cc8a7c17f46e7ebeeef4a00f55ff7ffdb4aa
cherylwu <1011077596@qq.com>
2023-10-30 23:44:29 +0800

    commit code about import

--- forrange.py
+++ forrange.py
@@ -1,3 +1,14 @@
+import random
+
 print('My name is:')
 for i in range(5):
     print('Jimmy Five Times ( ' + str(i) + ')')
+
+print('My name2 is:')
+k = 0
+while k < 5:
+    print('Jimmy Five Times ( ' + str(k) + ')')
+    k = k + 1
+
+for j in range(0, 20, 5):
+    print(j)
---
+++ printRandom.py
@@ -0,0 +1,10 @@
+import random, sys, os, math
+for i in range(5):
+    print(random.randint(1, 10))
+
+while True:
+    print('Type exit to exit.')
+    response = input()
+    if response == 'exit':
+        sys.exit()
+    print('You typed ' + response + '.')
