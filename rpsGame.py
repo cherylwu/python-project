ab21e8367fc91ec7424c555f20c48d481fa29983
cherylwu <1011077596@qq.com>
2023-10-31 13:53:05 +0800

    commit code about import

---
+++ rpsGame.py
@@ -0,0 +1,11 @@
+import random, sys
+
+print('ROCK, PAPER, SCISSORS')
+
+# These variables keep track of the number of wins, losses, ties.
+wins = 0
+losses = 0
+ties = 0
+
+while True:
+    print('%s Wins, %s Losses, %s Ties' % (wins, losses, ties))
