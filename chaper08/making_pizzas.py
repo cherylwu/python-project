06ecab164d7e7c7c1aaf9f444aea4583362045d9
cherylwu <1011077596@qq.com>
2023-11-14 23:54:45 +0800

    commit code about list **

--- chaper08/making_pizzas.py
+++ chaper08/making_pizzas.py
@@ -1,11 +1,17 @@
 """import pizza
+#from pizza import *
 
 pizza.make_pizza3(16, 'pepperoni')
 pizza.make_pizza3(12, 'mushroom', 'green peppers', 'extra cheese')
 
-"""
 
 from pizza import make_pizza3 as mp
 
 mp(16, 'pepperoni')
 mp(12, 'mushroom', 'green peppers', 'extra cheese')
+"""
+
+import pizza as p
+
+p.make_pizza3(16, 'pepperoni')
+p.make_pizza3(12, 'mushroom', 'green peppers', 'extra cheese')
